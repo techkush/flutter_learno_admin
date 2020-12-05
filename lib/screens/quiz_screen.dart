6b69b93c6d9b5991372f00828d19c0e0f9a0aa75
Namesh Kushantha <44038404+techkush@users.noreply.github.com>
2020-12-05 07:45:00 +0530

    Quiz UI completed

--- lib/screens/quiz_screen.dart
+++ lib/screens/quiz_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_learno_admin/widgets/progress.dart';
-import 'package:uuid/uuid.dart';
 
 class QuizScreen extends StatefulWidget {
   @override
@@ -21,6 +20,7 @@ class _QuizScreenState extends State<QuizScreen> {
   bool _isThereAMessage = false;
   String _message;
   Color _messageColor = Colors.green;
+  bool isCorrectAnswer = false;
 
   @override
   Widget build(BuildContext context) {
@@ -71,7 +71,7 @@ class _QuizScreenState extends State<QuizScreen> {
             SizedBox(
               height: 20,
             ),
-            //formFill(),
+            formFill(),
             SizedBox(
               height: 20,
             ),
@@ -79,6 +79,126 @@ class _QuizScreenState extends State<QuizScreen> {
           ],
         ),
       ),
+    );
+  }
+
+  Widget formFill() {
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Container(
+              width: 300,
+              child: TextFormField(
+                controller: topicController,
+                onChanged: (value) {
+                  setState(() {
+                    _topicId = value;
+                  });
+                },
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), hintText: 'Topic Id'),
+              ),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Container(
+              width: 300,
+              child: TextFormField(
+                controller: questionController,
+                onChanged: (value) {
+                  setState(() {
+                    _question = value;
+                  });
+                },
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), hintText: 'Question'),
+              ),
+            ),
+            Container(
+              width: 200,
+              height: 70,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  height: 40,
+                  decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(30)),
+                  width: MediaQuery.of(context).size.width - 30,
+                  child: RaisedButton(
+                    child: Text(
+                      'Add Question',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: Colors.blue,
+                    onPressed: () async {
+
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        // Answers
+        Row(
+          children: <Widget>[
+            InkWell(
+              onTap: (){
+                setState(() {
+                  isCorrectAnswer = !isCorrectAnswer;
+                });
+              },
+              child: Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: isCorrectAnswer ? Colors.green : Colors.red,
+                  borderRadius: BorderRadius.circular(10)
+                ),
+              ),
+            ),
+            SizedBox(width: 10,),
+            Container(
+              width: 562,
+              child: TextFormField(
+                controller: answerController,
+                onChanged: (value) {
+                  setState(() {
+                    _answer = value;
+                  });
+                },
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), hintText: 'Question'),
+              ),
+            ),
+            Container(
+              width: 200,
+              height: 70,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  height: 40,
+                  decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(30)),
+                  width: MediaQuery.of(context).size.width - 30,
+                  child: RaisedButton(
+                    child: Text(
+                      'Add Answer',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: Colors.blue,
+                    onPressed: () {
+
+                    },
+                  ),
+                ),
+              ),
+            )
+          ],
+        )
+      ],
     );
   }
 }
